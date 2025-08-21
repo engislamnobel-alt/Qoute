cab17d68aba990b11483d72b574ba4f881a34cc5
engislamnobel-alt <islamnobel@gmail.com>
2025-08-21 18:34:47 +0200

    first commit

---
+++ Js/index.js
@@ -0,0 +1,19 @@
+var quotes = [
+  " “Be yourself; everyone else is already taken.”",
+  "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst then you sure as hell don't deserve me at my best.",
+  "“So many books, so little time.”",
+  " “Be yourself; everyone else is already taken.”",
+  "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst then you sure as hell don't deserve me at my best.",
+  "“So many books, so little time.”",
+];
+
+function getQoute() {
+
+    
+  var x = quotes.shift();
+  document.getElementById("demo").innerHTML = x;
+
+  
+
+}
+
